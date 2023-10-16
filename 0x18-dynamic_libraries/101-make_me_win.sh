b602ec6022b26404182a33b334239320051272c4
imanebenhadda <imanebenhaddapama@gmail.com>
2023-10-16 12:43:43 -0700

    add

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -N -P /tmp/win.so https://github.com/MD-MOUAD/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
+wget -N -P /tmp/win.so https://github.com/imanebenhadda/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/win.so
