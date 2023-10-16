15eb690394717871fc8099c5473e76ea79da1402
imanebenhadda <imanebenhaddapama@gmail.com>
2023-10-16 16:30:10 -0700

    all

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/imanebenhadda/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+wget -N -P /tmp/win.so https://github.com/imanebenhadda/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
